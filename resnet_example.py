5c8312f826de7366860bd9ce28ff4fadb363f544
RomanLeshchuk <romanleshchuk2000@gmail.com>
2026-03-04 22:54:51 +0200

    Added pytorch resnet distributed DL example

---
+++ resnet_example.py
@@ -0,0 +1,152 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import os
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, 
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = self.shortcut(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += identity
+        out = self.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self._make_layer(64, num_blocks=2, stride=1)
+        self.layer2 = self._make_layer(128, num_blocks=2, stride=2)
+        self.layer3 = self._make_layer(256, num_blocks=2, stride=2)
+        self.layer4 = self._make_layer(512, num_blocks=2, stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, out_channels, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for s in strides:
+            layers.append(ResidualBlock(self.in_channels, out_channels, s))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+def ddp_setup():
+    init_process_group(backend="nccl") # or "gloo" for CPU
+    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+
+def main():
+    ddp_setup()
+    rank = int(os.environ["LOCAL_RANK"])
+    device = torch.device(f"cuda:{rank}")
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root="./data", train=True, download=(rank==0), transform=transform_train)
+    train_sampler = DistributedSampler(trainset)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=False, sampler=train_sampler)
+    
+    testset = torchvision.datasets.CIFAR10(root="./data", train=False, download=(rank==0), transform=transform_test)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False)
+
+    model = ResNet(num_classes=10).to(device)
+    model = DDP(model, device_ids=[rank])
+    
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    epochs = 5
+    for epoch in range(epochs):
+        model.train()
+        train_sampler.set_epoch(epoch)
+        sum_loss = 0
+
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data[0].to(device), data[1].to(device)
+
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            sum_loss += loss.item()
+            if rank == 0 and (i + 1) % 10 == 0:
+                print(f"[Epoch {epoch + 1}, Batch {i + 1}] Loss: {(sum_loss / 10):.3f}")
+                sum_loss = 0
+
+    if rank == 0:
+        print("\nGradient matrices which need to be synchronized:\n")
+        for name, param in model.module.named_parameters():
+            if param.requires_grad and param.grad is not None:
+                print(f"{name}: {param.grad.shape}")
+
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                inputs, labels = data[0].to(device), data[1].to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print(f"\nAccuracy on test set: {correct / total * 100:.2f}%")
+
+    destroy_process_group()
+
+if __name__ == "__main__":
+    main()
